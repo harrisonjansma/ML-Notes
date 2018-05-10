66a38b90f63cdd8f6ee4e67241fd40cacba7d60d
Harrison Jansma <36687008+harrisonjansma@users.noreply.github.com>
2018-05-09 20:31:46 -0500

    5/9 updates

--- VisualFuncs.py
+++ VisualFuncs.py
@@ -133,4 +133,32 @@ def OneClassVDR(X, y, clf, resolution = 0.02):
     plt.show()
  
 
+def plot_svc_decision_function(model, ax=None, plot_support=True):
+    """Plot the decision function for a 2D SVC"""
+    if ax is None:
+        ax = plt.gca()
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+    
+    # create grid to evaluate model
+    x = np.linspace(xlim[0], xlim[1], 30)
+    y = np.linspace(ylim[0], ylim[1], 30)
+    Y, X = np.meshgrid(y, x)
+    xy = np.vstack([X.ravel(), Y.ravel()]).T
+    P = model.decision_function(xy).reshape(X.shape)
+    
+    # plot decision boundary and margins
+    ax.contour(X, Y, P, colors='k',
+               levels=[-1, 0, 1], alpha=0.5,
+               linestyles=['--', '-', '--'])
+    
+    # plot support vectors
+    if plot_support:
+        ax.scatter(model.support_vectors_[:, 0],
+                   model.support_vectors_[:, 1],
+                   s=300, linewidth=1, facecolors='none');
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
     
